048c3fecd2a84f69bd419e17cb3f91c0b889c6ba
akbar6706154097 <akbarwijayaa@gmail.com>
2017-04-06 09:20:23 +0700

    Update Main.java

--- jurnal2/src/com/company/Main.java
+++ jurnal2/src/com/company/Main.java
@@ -7,11 +7,11 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         Scanner scn = new Scanner(System.in);
-        System.out.print("input tinggi : ");
+        System.out.print("input tinggi ketupat : ");
         int n = scn.nextInt();
 
         for (int i = 1; i < n; i += 2) {
-            for (int j = 0; j < 4 - i / 2; j++)
+            for (int j = 0; j < 6 - i / 2; j++)
                 System.out.print(" ");
 
             for (int j = 0; j < i; j++)
@@ -20,12 +20,13 @@ public class Main {
         }
 
         for (int i = n; i > 0; i -= 2) {
-            for (int j = 0; j < 4 - i / 2; j++)
+            for (int j = 0; j < 6 - i / 2; j++)
                 System.out.print(" ");
 
             for (int j = 0; j < i; j++)
                 System.out.print("*");
             System.out.print("\n");
         }
+        // Update Ketupat
     }
 }
